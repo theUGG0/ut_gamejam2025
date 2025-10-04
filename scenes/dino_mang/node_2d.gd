260f86d4aa8365211ef3aab720f3ce0ada7ff5ce
Alicja Ainsoo <alicja.ainsoo@gmail.com>
2025-10-04 12:50:22 +0300

    dino aint pls

---
+++ scenes/dino_mang/node_2d.gd
@@ -0,0 +1,38 @@
+extends AnimatedSprite2D
+
+@export var speed = 400
+var screen_size 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed('ui_right'):
+		velocity.x +=1
+	elif Input.is_action_pressed('ui_left'):
+		velocity.x -=1
+	elif Input.is_action_pressed('ui_down'):
+		velocity.y +=1
+	elif Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() *speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+	# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
