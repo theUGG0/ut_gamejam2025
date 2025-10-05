3f335f3a1e9a86fe10e5f08ae228d9fb8b4dba4d
hellhoundio <123471209+hellhoundio@users.noreply.github.com>
2025-10-05 12:17:24 +0300

    Add files via upload
    laburint on olemas

---
+++ labyrnithty/coin_6.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+signal collected
+
+var positions = [Vector2(859.0,111.0),Vector2(630.0,144.0)]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = positions.pick_random()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	$Sprite2D.visible = false
+	emit_signal("collected")
+	queue_free()
